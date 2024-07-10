f97c1c95cd2710360cdae482d3e1801d813c77c3
Alexandr <belmymail@gmail.com>
2024-07-10 11:54:47 +0300

    ssl cert path fixed

--- generate_cert.sh
+++ generate_cert.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-openssl req -x509 -nodes -newkey rsa:2048 -keyout ./nginx/cert/key.pem -out ./nginx/cert/cert.pem -sha256 -days 365 \
+openssl req -x509 -nodes -newkey rsa:2048 -keyout ./nginx/key.pem -out ./nginx/cert.pem -sha256 -days 365 \
     -subj "/C=GB/ST=London/L=London/O=nc1/OU=IT Department/CN=YOUR_DOMAIN_NAME"
